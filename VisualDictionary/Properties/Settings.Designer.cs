0a76286c324c62fcd61ee0a1eef9290fb5286cf6
Luong Hoang <neverthelimit@outlook.com>
2012-10-30 22:51:28 -0400

    Changed code to default the popup window to pinned mode so it doesn't go away

--- VisualDictionary/Properties/Settings.Designer.cs
+++ VisualDictionary/Properties/Settings.Designer.cs
@@ -85,7 +85,7 @@ namespace VisualDictionary.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool WindowPinned {
             get {
                 return ((bool)(this["WindowPinned"]));
